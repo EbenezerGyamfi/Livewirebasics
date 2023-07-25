868ef8dc17cc2d037503f39aad0deab62c09acd0
eben <ebenezer.gyamfi@regent.edu.gh>
2023-07-25 10:34:57 +0000

    search filter from apple api

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+
+
+    public $search;
+    public $searchResults = [];
+
+    public function updatedSearch($value)
+    {
+
+        if (strlen($this->search) < 3) {
+
+            return $this->searchResults = [];
+        }
+        $response = Http::get('https://itunes.apple.com/search?term=' . $this->search . '&limit=10');
+
+        $this->searchResults = $response->json()['results'];
+    }
+
     public function render()
     {
+        // dd($response->json()['results']);
         return view('livewire.search-dropdown');
     }
 }
